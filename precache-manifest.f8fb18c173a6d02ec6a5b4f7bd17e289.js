a66ea203a74c41a94a275811596ab9c25e482c23
excaliburne <jeremy.faret@gmail.com>
2020-06-22 06:26:31 +0300

    Updates

---
+++ precache-manifest.f8fb18c173a6d02ec6a5b4f7bd17e289.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83153047fde7ae75fddf316220e5f170",
+    "url": "/react_monsters-rolodex/index.html"
+  },
+  {
+    "revision": "57b37b2fe6c806bd6794",
+    "url": "/react_monsters-rolodex/static/css/main.2225f6cf.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/react_monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57b37b2fe6c806bd6794",
+    "url": "/react_monsters-rolodex/static/js/main.c62221c8.chunk.js"
+  },
+  {
+    "revision": "3952a70d77276c213bce",
+    "url": "/react_monsters-rolodex/static/js/runtime-main.22d4c08b.js"
+  }
+]);
